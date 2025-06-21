f080b1ee95113a9943a2c5107b694c482bf0c6e7
darkwebbd404 <darkwebbd404@gmail.com>
2025-06-21 12:27:15 +0600

    Update GetJwtTokenByApi.py

--- GetJwtTokenByApi.py
+++ GetJwtTokenByApi.py
@@ -1,5 +1,5 @@
 import requests
-url = "https://projects-fox-x-get-jwt.vercel.app/get?3805642913=&password=BB43313F3419F8961BABF036994BD8DE638E20AE9FEB59348318B93056725DF8"
+url = "https://jwt-ei3z.onrender.com/api/oauth_guest?uid=3987815200&passwordB17E6BB8C9046D75DE76146FD7730470BB94AE42EA37D40D5C22313689E874F6"
 get = requests.get(url).json()
 data = get["token"]
 print(data)
